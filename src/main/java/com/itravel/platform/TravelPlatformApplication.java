cc34be4f655908cfddc7d4c5f46d92d112964003
Đặng Duy Minh <dangduyminh109@gmail.com>
2026-02-16 14:51:56 +0700

    initialize project

---
+++ src/main/java/com/itravel/platform/TravelPlatformApplication.java
@@ -0,0 +1,12 @@
+package com.itravel.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelPlatformApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TravelPlatformApplication.class, args);
+	}
+
+}
